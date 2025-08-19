e335af0487b903f2cfb6f69314aba0bf2fb12ea4
pullusdoctus <ja.rojas.dev@gmail.com>
2025-08-19 13:53:10 -0600

    test with whale ASCII art

--- IP/classwork/01:20250812/ipv4-test.cc
+++ IP/classwork/01:20250812/ipv4-test.cc
@@ -9,11 +9,12 @@ int main( int argc, char * argv[] ) {
   const char* ose = "163.178.104.62";
   const char* request = "GET /pirofs/index.php?disk=Disk-01&cmd=ls HTTP/1.1"
     "\r\nhost: redes.ecci\r\n\r\n";
+  const char* whale = "GET /aArt/index.php?disk=Disk-01&fig=whale-1.txt\r\nHTTP/v1.1\r\nhost: redes.ecci\r\n\r\n";
   VSocket * s;
   char a[512];
   s = new Socket('s');
   s->MakeConnection( osi, 80 );
-  s->Write(  request );
+  s->Write(  whale );
   s->Read( a, 512 );
   printf( "%s\n", a);
 }
