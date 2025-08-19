5692dbb9aed5835a9f7099df26b2e1371a96fef5
pullusdoctus <ja.rojas.dev@gmail.com>
2025-08-19 14:09:57 -0600

    add interfaces

--- IP/classwork/02:20250819/VSocket.h
+++ IP/classwork/02:20250819/VSocket.h
@@ -20,15 +20,20 @@ class VSocket {
 
   void BuildSocket(char type, bool IPv6 = false );
   void Close();
+
   int EstablishConnection(const char* host, int port);
   int EstablishConnection(const char* host, const char* service);
   virtual int MakeConnection(const char* host, int port) = 0;
   virtual int MakeConnection(const char* host, const char* service) = 0;
-
   virtual size_t Connect(const char* host, int port) = 0;
+
   virtual size_t Read(void* buffer, size_t bufferSize) = 0;
   virtual size_t Write(const void* buffer, size_t bufferSize) = 0;
   virtual size_t Write(const char* text) = 0;
+
+  int Bind(int);
+  size_t SendTo(const void*, size_t, void*);
+  size_t ReceiveFrom(void*, size_t, void*);
 
  protected:
   bool IPv6;      // Is IPv6 socket?
