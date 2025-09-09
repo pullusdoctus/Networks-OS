ab7c77b026a5b23c58073861692a06f676ab30cf
Laboratorio-3-5 <lab-3-5@lab-3-5-WS-18.ecci.ucr.ac.cr>
2025-09-09 15:04:00 -0600

    init classwork for 2025-09-09

---
+++ IP/classwork/07:20250909/Socket.h
@@ -0,0 +1,29 @@
+/**
+  *  Universidad de Costa Rica
+  *  ECCI
+  *  CI0123 Proyecto integrador de redes y sistemas operativos
+  *  2025-i
+  *  Grupos: 1 y 3
+  *
+  ****** Socket class interface
+  *
+  * (Fedora version)
+ **/
+
+#pragma once
+#include "VSocket.h"
+
+class Socket : public VSocket {
+   public:
+      Socket(char type, bool IPv6 = false);
+      ~Socket();
+      size_t Connect(const char* host, int port) override;
+      int MakeConnection(const char* host, int port) override;
+      int MakeConnection(const char* host, const char* service) override;
+      size_t Read(void* buffer, size_t bufferSize) override;
+      size_t Write(const void* buffer, size_t bufferSize) override;
+      size_t Write(const char* text) override;
+      int Listen(int connections = 5);
+      VSocket* AcceptConnection();
+};
+
